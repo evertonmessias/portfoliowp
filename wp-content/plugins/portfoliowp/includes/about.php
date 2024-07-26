51fb83b08f013ed109dce8495e66a835c8d325b2
evertonmessias <everton.messias@gmail.com>
2024-07-26 16:33:42 -0300

    26-07-2024_16:33:42

--- wp-content/plugins/portfoliowp/includes/about.php
+++ wp-content/plugins/portfoliowp/includes/about.php
@@ -3,7 +3,7 @@
     <p>É um plugin para gerenciamento do site <b>portfoliowp</b>.</p>
     <ul>
         <li><a href="https://github.com/evertonmessias/portfoliowp" target="_blank">Projeto</a></li>
-        <li><a href="https://portfolio.evertonm.com/" target="_blank">Site do Desenvolvedor</a></li>
+        <li><a href="https://evertonm.com.br" target="_blank">Site do Desenvolvedor</a></li>
     </ul>
     <br>    
 </div>
